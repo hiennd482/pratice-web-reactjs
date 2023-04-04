c1edd27da17af33329c42d8200f851d3857b00fc
hiennd482 <fayolo482@gmail.com>
2023-04-04 17:35:14 +0700

    'completetest

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 
 #3:cd vào thư mục selenium trong terminal (..\src\selenium)
 
-#4:khi đã vào đúng đường dẫn như trên thì chạy câu lệnh "node testfail.js" hoặc "node testpass.js"
+#4:khi đã vào đúng đường dẫn như trên thì chạy câu lệnh 'npm test'
+
+<!-- CÁCH TEST TRÊN TRÌNH DUYỆT FIRE FOX -->
+
+#1 : npm i geckodriver
+#2: require("geckodriver") vào file testall.test.js
+#3: thay đổi 'chrome' thành 'firefox' trong forBrowser() vd : "forBrowser('firefox')"
 
 <!-- HẾT! -->
---
+++ src/selenium/testall.test.js
@@ -0,0 +1,56 @@
+const { Builder, Browser, By, Key, until } = require("selenium-webdriver");
+require("chromedriver");
+require("geckodriver");
+
+describe("webdriver", () => {
+  let driver;
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser("firefox").build();
+    // driver = await new Builder().forBrowser("chrome").build();
+  }, 10000);
+
+  test("check error", async () => {
+    await driver.get("http://localhost:3000/login?");
+    const username = await driver.findElement(By.name("login")).sendKeys("");
+    await driver
+      .findElement(By.name("password"))
+      .sendKeys("********", Key.RETURN);
+
+    expect(username).toBe(null);
+  });
+  test("check navigate", async () => {
+    await driver.get("http://localhost:3000/login?");
+    const navi = await driver.findElement(By.name("login")).sendKeys("hien482");
+    await driver
+      .findElement(By.xpath(`//*[@id="root"]/div/div/div/div[2]/form/input[2]`))
+      .sendKeys("********", Key.RETURN);
+
+    // const checkEl = await driver.findElement(By.className("fortest"));
+
+    expect(navi).toBeInTheDocument();
+  });
+  setInterval(() => {
+    driver.quit();
+  }, 10000);
+  // afterAll(async () => {
+  //   await driver.quit();
+  // }, 10000);
+});
+// const test_case = async () => {
+
+//   await driver.get("http://localhost:3000/login?");
+//   //   await driver.findElement(By.name("q")).sendKeys("hello world", Key.RETURN);
+//   await driver.findElement(By.name("login")).sendKeys("hien482");
+//   await driver
+//     .findElement(By.name("password"))
+//     .sendKeys("********", Key.RETURN);
+//   if (await driver.findElement(By.className("fortest")).isDisplayed()) {
+//     console.log("test  success");
+//   } else {
+//     console.log("test failed");
+//   }
+//   setInterval(() => {
+//     driver.quit();
+//   }, 10000);
+// };
+// test_case();
