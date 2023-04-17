cd97e3e4d6774591ade5be933c910c3bf59367a1
hiennd482 <fayolo482@gmail.com>
2023-04-17 17:24:47 +0700

    'btntoggle'

--- src/pages/testdata/renderdata.jsx
+++ src/pages/testdata/renderdata.jsx
@@ -1,77 +1,121 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./index.scss";
+function Renderdata() {
+  useEffect(() => {
+    const accordionHeader = document.querySelectorAll(".accordion-header");
+    accordionHeader.forEach((header) => {
+      header.addEventListener("click", function () {
+        const accordionContent =
+          header.parentElement.querySelector(".accordion-content");
+        let accordionMaxHeight = accordionContent.style.maxHeight;
 
-function renderdata() {
-  const data = [
-    {
-      id: 1,
-      name: "Item 1",
-      childs: [
-        { id: 11, name: "Item 1.1" },
-        { id: 12, name: "Item 1.2" },
-      ],
-    },
-    {
-      id: 2,
-      name: "Item 2",
-      childs: [
-        { id: 21, name: "Item 2.1" },
-        { id: 22, name: "Item 2.2" },
-        {
-          id: 23,
-          name: "Item 2.3",
-          childs: [
-            { id: 231, name: "Item 2.3.1" },
-            { id: 232, name: "Item 2.3.2" },
-            { id: 233, name: "Item 2.3.3" },
-            {
-              id: 234,
-              name: "Item 2.3.4",
-              childs: [
-                { id: 2341, name: "Item 2.3.4.1" },
-                {
-                  id: 2342,
-                  name: "Item 2.3.4.2",
-                  childs: [{ id: 5555, name: "item 55555" }],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: "Item 3",
-      childs: [{ id: 31, name: "Item 3.1" }],
-    },
-    {
-      id: 4,
-      name: "Item 4",
-    },
-  ];
+        // Condition handling
+        if (accordionMaxHeight === "0px" || accordionMaxHeight.length === 0) {
+          accordionContent.style.maxHeight = `${
+            accordionContent.scrollHeight + 32
+          }px`;
+          header.querySelector(".fas").classList.remove("fa-plus");
+          header.querySelector(".fas").classList.add("fa-minus");
+          header.parentElement.classList.add("bg-indigo-50");
+        } else {
+          accordionContent.style.maxHeight = `0px`;
+          header.querySelector(".fas").classList.add("fa-plus");
+          header.querySelector(".fas").classList.remove("fa-minus");
+          header.parentElement.classList.remove("bg-indigo-50");
+        }
+      });
+    });
+  });
 
-  const RecursiveComponent = ({ data }) => {
-    return (
-      <div>
-        {data.map((i) => (
-          // console.log(i.childs);
-          <ul>
-            <li>
-              {i.id},{i.name}
-              {i.childs && (
-                <RecursiveComponent data={i.childs}></RecursiveComponent>
-              )}
-            </li>
-          </ul>
-        ))}
-      </div>
-    );
-  };
   return (
-    <div>
-      <RecursiveComponent data={data}></RecursiveComponent>
+    <div class="h-screen bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center">
+      <div class="w-6/12 mx-auto rounded border">
+        <div class="bg-white p-10 shadow-sm">
+          <h3 class="text-lg font-medium text-gray-800">
+            Several Windows stacked on each other
+          </h3>
+          <p class="text-sm font-light text-gray-600 my-3">
+            The accordion is a graphical control element comprising a vertically
+            stacked list of items such as labels or thumbnails
+          </p>
+
+          <div class="h-1 w-full mx-auto border-b my-5"></div>
+
+          <div class="transition hover:bg-indigo-50">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i class="fas fa-plus"></i>
+              <h3>What is term?</h3>
+            </div>
+
+            <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+              <p class="leading-6 font-light pl-9 text-justify">
+                Our asked sex point her she seems. New plenty she horses parish
+                design you. Stuff sight equal of my woody. Him children bringing
+                goodness suitable she entirely put far daughter.
+              </p>
+              <button class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">
+                Learn more
+              </button>
+            </div>
+          </div>
+
+          <div class="transition hover:bg-indigo-50">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i class="fas fa-plus"></i>
+              <h3>When to use Accordion Components?</h3>
+            </div>
+
+            <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+              <p class="leading-6 font-light pl-9 text-justify">
+                Our asked sex point her she seems. New plenty she horses parish
+                design you. Stuff sight equal of my woody. Him children bringing
+                goodness suitable she entirely put far daughter.
+              </p>
+              <button class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">
+                Learn more
+              </button>
+            </div>
+          </div>
+
+          <div class="transition hover:bg-indigo-50">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i class="fas fa-plus"></i>
+              <h3>How can it be defined?</h3>
+            </div>
+
+            <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+              <p class="leading-6 font-light pl-9 text-justify">
+                Our asked sex point her she seems. New plenty she horses parish
+                design you. Stuff sight equal of my woody. Him children bringing
+                goodness suitable she entirely put far daughter.
+              </p>
+              <button class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">
+                Learn more
+              </button>
+            </div>
+          </div>
+
+          <div class="transition hover:bg-indigo-50">
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i class="fas fa-plus"></i>
+              <h3>Chamber reached do he nothing be?</h3>
+            </div>
+
+            <div class="accordion-content px-5 pt-0 overflow-hidden max-h-0">
+              <p class="leading-6 font-light pl-9 text-justify">
+                Our asked sex point her she seems. New plenty she horses parish
+                design you. Stuff sight equal of my woody. Him children bringing
+                goodness suitable she entirely put far daughter.
+              </p>
+              <button class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">
+                Learn more
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default renderdata;
+export default Renderdata;
